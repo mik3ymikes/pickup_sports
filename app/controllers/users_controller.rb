92afde8c9eefa993d8c1a0a6743f62d86e3c3099
mik3ymikes <mikey.henleys@gmail.com>
2024-01-31 20:56:11 -0600

    completed create and  update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+before_action :set_user, only:[:show, :update]
+
   def index
     users=User.all 
 
@@ -6,8 +8,38 @@ class UsersController < ApplicationController
   end
 
   def show
-    user=User.find(params[:id])
+    render json: @user, status: 200
+  end
 
-    render json:user, status: 200
+  def create
+   user=User.new(user_params)
+
+   if user.save
+    render json: user, status: :created
+   else
+    render json: user.errors, status: :unprocessable_entity
+   end
+  end
+
+  def update
+  
+
+    if @user.update(user_params)
+      render json: @user, status: :ok
+    else
+         render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+
+
+  private
+
+  def set_user
+    @user=User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :first_name, :last_name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
 get '/users', to: 'users#index'
 get '/users/:id', to: 'users#show'
 
+post '/users', to: 'users#create'
+
+
+put '/users/:id', to: 'users#update' 
 end
