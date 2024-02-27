a62decf64a91a42714bebcdb64ff329debe78434
mik3ymikes <mikey.henleys@gmail.com>
2024-02-27 10:37:56 -0600

    setting up web controller and routes

--- app/blueprints/user_blueprint.rb
+++ app/blueprints/user_blueprint.rb
@@ -2,7 +2,11 @@
 
 class UserBlueprint < Blueprinter::Base
     identifier :id
-
+    
+    view :me do
+        fields :first_name, :last_name, :username, :email
+    end
+    
     view :normal do
         fields :username
     end
