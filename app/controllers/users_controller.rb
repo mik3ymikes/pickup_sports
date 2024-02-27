3f7a1684847928ad66c01c1513029b80e2350af0
mik3ymikes <mikey.henleys@gmail.com>
2024-02-27 16:27:47 -0600

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ before_action :authenticate_request, only: [:index, :show, :update, :destroy]
    if user.save
     render json: user, status: :created
    else
-    render json: user.errors, status: :unprocessable_entity
+    render json: user.errors.full_messages, status: :unprocessable_entity
    end
   end
 
